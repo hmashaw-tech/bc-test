57c40c1d0315a07feb0daf4cde4eaf5072e80290
hmashaw <hmashaw@gmail.com>
2018-03-03 08:33:52 -0500

    Functional Proof of Work
    - index.js - Additional demo code
    - Block.js - Add UTC Timestamp
    - Blockchain.js - Add mining for genesis Block
    - bc-test.js - Add PoW test

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const Blockchain = require('./src/Blockchain')
 
 let demoBlockchain = new Blockchain()
 
-demoBlockchain.addBlock(new Block(1, '1/1/1970', { amount: 100 }))
-demoBlockchain.addBlock(new Block(2, '1/1/1971', { amount: 500 }))
+console.log('Mining block #1...')
+demoBlockchain.addBlock(new Block(1, 'X', { amount: 100 }))
+
+console.log('Mining block #2...')
+demoBlockchain.addBlock(new Block(2, 'X', { amount: 500 }))
 
 console.log(`Is the current blockchain valid? ${demoBlockchain.isChainValid()}`)
 
 console.log(JSON.stringify(demoBlockchain, null, 4))
+
+let dt = new Date()
+let utcDate = dt.toUTCString()
+console.log(utcDate)
 
--- src/Block.js
+++ src/Block.js
@@ -10,15 +10,34 @@ class Block {
                 data,           // Well, data
                 pbHash=''){     // Hash of block before this block
         
+        if (timestamp === 'X') {
+            let dt = new Date()
+            let utcDate = dt.toUTCString()
+            timestamp = utcDate
+        }
+
         this.index = index
         this.timestamp = timestamp
         this.data = data
         this.pbHash = pbHash
         this.hash = this.hashMe()
+        this.nonce = 0          // Just a value used to aid hash creation when mining
     }
 
     hashMe(){
-        return SHA256(this.index + this.timestamp + this.pbHash + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.timestamp + this.pbHash + this.nonce + JSON.stringify(this.data)).toString()
+    }
+
+    mineMe(difficulty){
+        // We need a string of 0s that is length = difficulty
+        const difficultyString = Array(difficulty + 1).join('0')
+
+        // While the block's hash does not start with the difficulty string ...
+        while(this.hash.substr(0, difficulty) !== difficultyString) {
+            this.nonce++
+            this.hash = this.hashMe()
+        }
+        // console.log(`Block mined: ${this.hash} -- Nonce: ${this.nonce}`)
     }
 }
 
